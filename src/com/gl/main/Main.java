3b2d6539fb312fb1161c20e9b3fe47d4faf6f467
Ananya Sahai <111261005+sahaiananya@users.noreply.github.com>
2022-10-11 10:21:13 +0530

    Add files via upload

---
+++ src/com/gl/main/Main.java
@@ -0,0 +1,32 @@
+package com.gl.main;
+
+import java.util.Scanner;
+
+import com.gl.transaction.Transaction;
+
+public class Main {
+	public static void main(String[] args) {
+		Transaction transaction = new Transaction();
+		Scanner scanner = new Scanner(System.in);
+		int sizeOfArray;
+		System.out.println("enter the size of transaction array");
+		sizeOfArray = scanner.nextInt();
+		System.out.println("enter the value of array");
+		int[] array =new int[sizeOfArray];
+		for (int i=0; i < sizeOfArray; i++) {
+			array[i]=scanner.nextInt();
+		}
+	System.out.println("enter the total number of targets that need to be achieved");
+	int target =scanner.nextInt();
+	for (int i=0;i<target;i++) {
+		System.out.println("enter the value of target");
+		int value = scanner.nextInt();
+		transaction.isTargetAchievable(value, array);
+	
+	}
+		
+	}
+	
+	
+
+}
---
+++ src/com/gl/transaction/Transaction.java
@@ -0,0 +1,18 @@
+package com.gl.transaction;
+
+public class Transaction {
+	public void isTargetAchievable(int target, int[] arr) {
+		int sum=0;
+		for (int i=0; i <arr.length; i++) {
+			sum=sum+arr[i];
+			if(sum>= target) {
+				System.out.println("Target achieved after" +(i+1)+"transactions");
+				break;
+			}
+		}
+		
+		if(sum<target) {
+			System.out.println("Target not acheved");
+		}
+	}
+}
